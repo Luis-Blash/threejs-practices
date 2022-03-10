3277464614dfc11e6dd38db79348085c9b16ec7b
Luis Enrique Ocampo Gonzalez <blash3g@gmail.com>
2022-03-09 21:40:33 -0600

    feat: add cube

--- metavers/src/index.js
+++ metavers/src/index.js
@@ -8,8 +8,25 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-//* Animate render
+//* Cubo
+const geometry = new THREE.BoxGeometry();
+//* Su material del cubo
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+//* Justamos
+const cube = new THREE.Mesh( geometry, material );
+//* Se añade el cubo
+scene.add( cube );
+//* Desde la posición de la camara
+camera.position.z = 5;
+
+
+
+//? Animate render --- Bucle constante
 function animate() {
+    cube.rotation.x += 0.01
+    cube.rotation.y += 0.01
+    
+    //* Para la animación del frame
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
 }
